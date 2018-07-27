cf4ff2271e4009a0d158ee1ed9bb237dc1f5a50f
ercohen14 <ercohen14@gmail.com>
2018-07-27 14:37:50 -0400

    Fix "Add Story" button

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -17,7 +17,7 @@ class StoriesController < ApplicationController
       redirect_to project_path(@project.id)
     else
       flash[:error] = @story.errors.full_messages
-      render edit_project_story_path(@project)
+      render :edit
     end
   end
 
@@ -28,7 +28,7 @@ class StoriesController < ApplicationController
       redirect_to project_path(@project.id)
     else
       flash[:error] = @story.errors.full_messages
-      render new_project_story_path(@project)
+      render :new
     end
   end
 
