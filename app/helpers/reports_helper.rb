5fa680b86c71d20c1fa13f2817ffe2568231a690
ercohen14 <ercohen14@gmail.com>
2018-08-15 16:54:40 -0300

    Remove average and sum when 2 estimates have not been uploaded

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,2 +1,19 @@
 module ReportsHelper
+
+  def best_estimate_for_story(story)
+    if story.estimates.length < 2
+      "Not enough estimates"
+    else
+      story.best_estimate_average
+    end
+  end
+
+  def worst_estimate_for_story(story)
+    if story.estimates.length < 2
+      "Not enough estimates"
+    else
+      story.worst_estimate_average
+    end
+  end
+
 end
--- app/models/story.rb
+++ app/models/story.rb
@@ -6,13 +6,17 @@ class Story < ApplicationRecord
   has_many :estimates
 
   def best_estimate_average
-    best_case = self.estimates.pluck(:best_case_points)
-    best_case.sum / best_case.length
+    return 0 if self.estimates.length < 2
+
+    best_cases = self.estimates.pluck(:best_case_points)
+    best_cases.sum / best_cases.length
   end
 
   def worst_estimate_average
-    worst_case = self.estimates.pluck(:worst_case_points)
-    worst_case.sum / worst_case.length
+    return 0 if self.estimates.length < 2
+
+    worst_cases = self.estimates.pluck(:worst_case_points)
+    worst_cases.sum / worst_cases.length
   end
 
 end
