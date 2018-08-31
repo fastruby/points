3bdf1f66de6bb33a48d29f79081887487f0e707d
ercohen14 <ercohen14@gmail.com>
2018-08-31 16:07:45 -0300

    Change reports page to show decimals

--- app/models/story.rb
+++ app/models/story.rb
@@ -9,14 +9,14 @@ class Story < ApplicationRecord
     return 0 if self.estimates.length < 2
 
     best_cases = self.estimates.pluck(:best_case_points)
-    best_cases.sum / best_cases.length
+    best_cases.sum / best_cases.length.to_f
   end
 
   def worst_estimate_average
     return 0 if self.estimates.length < 2
 
     worst_cases = self.estimates.pluck(:worst_case_points)
-    worst_cases.sum / worst_cases.length
+    worst_cases.sum / worst_cases.length.to_f
   end
 
 end
