960aebbdca7b15ec6750351e524322d782fdb125
ercohen14 <ercohen14@gmail.com>
2018-09-18 15:49:05 -0300

    Add Reports Controller tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,8 @@
 FactoryBot.define do
   factory :user do
-    sequence(:email) {|n| "test#{n}@gmail.com" } 
+    sequence(:email) {|n| "test#{n}@gmail.com" }
     password "123456"
+    
+    trait(:admin)  { admin  true }
   end
 end
