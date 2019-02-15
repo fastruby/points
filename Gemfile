e2c0a8f66d2623ec1da0a52a98d0ad1600d0283c
Emily Cohen <emily@mtairy.local>
2019-02-15 10:33:34 -0300

    Add redcarpet and apply to story.description

--- Gemfile
+++ Gemfile
@@ -20,7 +20,7 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
-
+gem 'redcarpet', '~> 3.0.0'
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+
+    def markdown(text)
+      options = {
+        filter_html:     true,
+        hard_wrap:       true,
+        link_attributes: { rel: 'nofollow', target: "_blank" },
+        fenced_code_blocks: true,
+        no_intra_emphasis:  true,
+      }
+
+      extensions = {
+        autolink:           true,
+        superscript:        true,
+      }
+
+      renderer = Redcarpet::Render::HTML.new(options)
+      markdown = Redcarpet::Markdown.new(renderer, extensions)
+
+      markdown.render(text).html_safe
+  end
+
 end
