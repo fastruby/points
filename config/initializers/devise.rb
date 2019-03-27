e5bd46e6dd0145ee882d885a28d49ecd5d137bc7
Emily Cohen <emily@mtairy.local>
2019-03-27 18:12:12 -0300

    Finish oauth setup

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -256,7 +256,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :github, ENV['APP_ID'], ENV['APP_SECRET'], scope: 'user,public_repo'
+  config.omniauth :github, ENV['APP_ID'], ENV['APP_SECRET'], scope: 'user:email'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
