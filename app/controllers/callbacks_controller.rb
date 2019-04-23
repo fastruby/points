8f5a5c1bf08c887081af6a1cabe38c2f5ba5a798
Emily Cohen <emily@mtairy.local>
2019-04-23 09:52:18 -0300

    Edit devise views

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -2,8 +2,6 @@ class CallbacksController < Devise::OmniauthCallbacksController
   def github
     company = request.env["omniauth.auth"]['extra']['raw_info']['company']
 
-    debugger
-
     if company == "@ombulabs"
       @user = User.from_omniauth(request.env["omniauth.auth"])
       sign_in_and_redirect @user
