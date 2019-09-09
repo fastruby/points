8c386a427389f84d50e4663b9d1936767bab547f
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2019-09-08 23:05:59 -0400

    Make sure we always have `rails-ujs` present in the asset pipeline
    `rails-ujs` is used by Rails to provide the magic that turns a `link_to "Delete", method: :delete` into the right kind of HTTP request
    
    When this dependency is not present, the app starts making a GET request instead of a DELETE request. This makes it impossible to delete stories, or anything...

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require rails-ujs
 //= require jquery
 //= require bootstrap-sprockets
 //= require jquery-ui/widget
