4de7e9850d686e44550042c0487632278ac502b1
Russell Jennings <violentpurr@gmail.com>
2019-09-19 12:16:58 -0400

    sets user.name to be omniauth name to fix reporting

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
        user.provider = auth.provider
        user.uid = auth.uid
        user.email = auth.info.email
+       user.name = auth.info.name
        user.password = Devise.friendly_token[0,20]
      end
    end
