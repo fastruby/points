39965c276a47a604e321b74b67cc8b470987f4b7
Emily <emily@ombulabs.com>
2019-10-04 17:40:39 -0300

    Update stories.rb

--- spec/factories/stories.rb
+++ spec/factories/stories.rb
@@ -2,8 +2,6 @@ FactoryBot.define do
   factory :story do
     title { Faker::ChuckNorris.fact }
     description { Faker::Marketing.buzzwords }
-    title { "This is a tester story" }
-    description { "This is the description" }
     real_score { 2 }
     project
   end
