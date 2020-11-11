67b7ee957ec2d822861c54465bca8d399f653c2d
bronzdoc <lsagastume1990@gmail.com>
2020-11-10 23:14:29 -0600

    Delete stories using ajax

--- app/assets/javascripts/project.js
+++ app/assets/javascripts/project.js
@@ -27,6 +27,27 @@ $(() => {
       }
     })
   })
+
+  $(document).on("click", ".delete", (e) => {
+    let element = $(e.target)
+
+    if (!confirm("Are you sure?")) { return }
+
+    let token = $("meta[name='csrf-token']").attr("content")
+    $.ajaxSetup({
+      beforeSend: function (xhr) {
+        xhr.setRequestHeader("X-CSRF-Token", token);
+      }
+    });
+
+    $.ajax({
+      url: element.data("delete-url"),
+      type: "DELETE",
+      error: (result) => {
+        console.log("There was an error destroying the story")
+      }
+    })
+  })
 })
 
 
