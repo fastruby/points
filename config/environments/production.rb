e21787c87f42c0f9c2ab0c04c2a8deaadb58a71e
Francois Buys <buys.fran@gmail.com>
2021-05-25 15:49:39 +0200

    Force HTTPS on all requests
    Closes: https://github.com/fastruby/points/issues/19
    
    Users should not be able to use point in production on http as it is not
    secure.
    
    With help from:
    https://bigbinary.com/blog/rails-5-adds-more-control-to-fine-tuning-ssl-usage

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,8 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
+  config.ssl_options = {  redirect: { status: 301 } }
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
