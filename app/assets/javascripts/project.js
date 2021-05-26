bf56b7e26b553de96d8c97fb882e17dea7d0b04f
G. Torres <gelsey@ombulabs.com>
2021-05-26 11:22:34 -0400

    Renames 'Delete' to 'Bulk Delete' and adds messages

--- app/assets/javascripts/project.js
+++ app/assets/javascripts/project.js
@@ -1,9 +1,23 @@
 $(() => {
+  $("input[name='stories[]']").click(() => {
+    let selected = $("input[name='stories[]']:checked");
+    let ending = selected.length == 1 ? "y" : "ies";
+    $(".number-of-stories-selected")
+      .text(`${selected.length} stor${ending} selected`)
+      .removeClass("alert");
+  })
+
   $("#bulk_delete").click(() => {
     let stories_ids = []
     $("input[name='stories[]']:checked").each((_, checkbox) => {
       stories_ids.push($(checkbox).val())
     })
+
+    if (stories_ids.length === 0) {
+      $(".number-of-stories-selected")
+        .text("Please select 1 or more stories")
+        .addClass("alert");
+    }
 
     let token = $("meta[name='csrf-token']").attr("content")
     $.ajaxSetup({
@@ -19,6 +33,7 @@ $(() => {
       success: () => {
         $(stories_ids).each((_, id) => {
           console.log(id)
+          $(".number-of-stories-selected").text("");
           $("#story_" + id).remove();
         })
       },
@@ -28,8 +43,3 @@ $(() => {
     })
   })
 })
-
-
-
-
-
