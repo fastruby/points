202e6dc7bb06dc3590ca9a6d638fe1becd75810b
G. Torres <gelsey@ombulabs.com>
2021-06-24 13:23:42 -0400

    adds span in view instead of adding and removing it to the h3

--- app/assets/javascripts/action_plan.js
+++ app/assets/javascripts/action_plan.js
@@ -2,9 +2,8 @@ document.addEventListener("turbolinks:load", function() {
   $("input[name='action-plan_prefix']").keyup(function(event) {
     const prefix = event.target.value.trim();
 
-    $("h3.action-plan_heading").each(function(index) {
-      $(this).children("span").remove();
-      $(this).prepend(`<span>${prefix}${prefix == "" ? "" : index + 1} </span>`);
+    $("h3.action-plan_heading > span").each(function(index) {
+      $(this).text(`${prefix}${prefix == "" ? "" : index + 1 } `);
     });
   });
 });
