8a08310bbdbef569ff1a31ba1aba51964fe3933b
Ariel Juodziukynas <arieljuod@gmail.com>
2021-10-14 17:33:10 -0300

    Prevent users interacting with other users' estimates

--- spec/controllers/estimates_controller_spec.rb
+++ spec/controllers/estimates_controller_spec.rb
@@ -10,20 +10,19 @@ RSpec.describe EstimatesController, type: :controller do
 
   before do
     @request.env["devise.mapping"] = Devise.mappings[:user]
-    user = FactoryBot.create(:user)
     sign_in user
   end
 
   describe "#new" do
     it "redirects to the new page" do
-      get :new, params: {id: estimate.id, story_id: story.id, project_id: project.id, user_id: user.id}
+      get :new, params: {id: estimate.id, story_id: story.id, project_id: project.id}
       expect(response).to render_template :new
     end
   end
 
   describe "#edit" do
     before do
-      get :edit, params: {id: estimate.id, story_id: story.id, project_id: project.id, user_id: user.id}
+      get :edit, params: {id: estimate.id, story_id: story.id, project_id: project.id}
     end
 
     it "redirects to the edit page" do
@@ -35,24 +34,36 @@ RSpec.describe EstimatesController, type: :controller do
     end
   end
 
+  describe "#edit as other user" do
+    it "disallows editing another users' estimate" do
+      user2 = FactoryBot.create(:user)
+      estimate2 = FactoryBot.create(:estimate, story: story, user: user2)
+
+      get :edit, params: {id: estimate2.id, story_id: story.id, project_id: project.id}
+
+      expect(response).to redirect_to project_path(story.project)
+      expect(flash[:error]).to eq "Estimate not found"
+    end
+  end
+
   describe "#create" do
     context "with valid attributes" do
-      let(:valid_params) {
-        {best_case_points: 1, worst_case_points: 3, story_id: story.id}
+      let(:estimate_params) {
+        {best_case_points: 1, worst_case_points: 3}
       }
 
       it "creates a new estimate" do
         expect {
           post :create, params: {project_id: project.id,
                                  story_id: story.id,
-                                 estimate: valid_params}
+                                 estimate: estimate_params}
         }.to change(Estimate, :count).by(1)
       end
 
       it "redirects to the new estimate" do
         post :create, params: {story_id: story.id,
                                project_id: project.id,
-                               estimate: valid_params}
+                               estimate: estimate_params}
 
         expect(response).to redirect_to project_path(project.id)
       end
@@ -83,6 +94,16 @@ RSpec.describe EstimatesController, type: :controller do
         delete :destroy, params: {id: estimate.id, story_id: story.id, project_id: project.id}
       }.to change(Estimate, :count).by(-1)
     end
+
+    it "disallows destroying another users' estimate" do
+      user2 = FactoryBot.create(:user)
+      estimate2 = FactoryBot.create(:estimate, story: story, user: user2)
+
+      delete :destroy, params: {id: estimate2.id, story_id: story.id, project_id: project.id}
+
+      expect(response).to redirect_to project_path(story.project)
+      expect(flash[:error]).to eq "Estimate not found"
+    end
   end
 
   describe "#update" do
@@ -93,6 +114,19 @@ RSpec.describe EstimatesController, type: :controller do
                             estimate: {best_case_points: "7", worst_case_points: "10"}}
 
       expect(estimate.reload.best_case_points).to eq 7
+    end
+
+    it "disallows updating another users' estimate" do
+      user2 = FactoryBot.create(:user)
+      estimate2 = FactoryBot.create(:estimate, story: story, user: user2)
+
+      put :update, params: {id: estimate2.id,
+                            story_id: story.id,
+                            project_id: project.id,
+                            estimate: {best_case_points: "7", worst_case_points: "10"}}
+
+      expect(response).to redirect_to project_path(story.project)
+      expect(flash[:error]).to eq "Estimate not found"
     end
   end
 end
