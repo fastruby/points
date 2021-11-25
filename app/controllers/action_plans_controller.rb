2945080055a350021580e5ca6ddff8c1d243951b
Ariel Juodziukynas <arieljuod@gmail.com>
2021-11-25 01:04:59 -0300

    Fix order query in the action plan too

--- app/controllers/action_plans_controller.rb
+++ app/controllers/action_plans_controller.rb
@@ -3,7 +3,7 @@ class ActionPlansController < ApplicationController
 
   def show
     @project = Project.find(params[:project_id])
-    @project_stories = @project.stories.order(:position)
+    @project_stories = @project.stories.order(:position, :created_at)
     @children = Project.where(parent_id: @project.id).includes(:stories).references(:stories).order("stories.position")
   end
 end
