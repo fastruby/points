b27f3caeb2a28db2cbdfd825d11dc7f46df004cc
ranendra <neutrino@ranendra.com.np>
2021-12-16 03:25:52 +0545

    Display sub-projects in project page.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -59,6 +59,7 @@ class ProjectsController < ApplicationController
 
   def show
     @project = Project.find(params[:id])
+    @sub_projects = @project.projects
     @stories = @project.stories.by_position.includes(:estimates)
     @siblings = @project.siblings
   end
