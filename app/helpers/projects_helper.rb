e60601ea908a16e0d599a9551b49a673d7adaadc
ranendra <neutrino@ranendra.com.np>
2021-12-18 03:13:07 +0545

    Use full width for the stories table if there are no sub-projects or siblings.

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -3,4 +3,8 @@ module ProjectsHelper
     @estimate_id = story.estimate_for(current_user)
     @estimate_id.present?
   end
+
+  def has_projects?(sub_projects, siblings)
+    sub_projects.any? || siblings.any?
+  end
 end
