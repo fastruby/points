867530ceebaf0cfffcccdec088b8f476996783d4
Ariel Juodziukynas <arieljuod@gmail.com>
2022-01-27 17:23:20 -0300

    refactor sleep

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -21,11 +21,12 @@ RSpec.describe "home specs" do
         first = find("a", text: sub_project1.title)
         last = find("a", text: sub_project3.title)
 
+        last.drag_to(first, html5: false)
+
+        expect(page).not_to have_selector("#project_#{project.id}.project-card.sorting")
+
         # we need some `sleep` calls around the dragging simulation because it uses a javascript
         # delay internally
-        last.drag_to(first, html5: false)
-        sleep(2)
-        expect(page).not_to have_selector("#project_#{project.id}.project-card.sorting")
         sleep(2)
 
         expect(sub_project1.reload.position).to eq 2
