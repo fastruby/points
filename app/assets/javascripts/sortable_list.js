469bcb71d0f253ae033d11d9db29a0c3963296b6
Ariel Juodziukynas <arieljuod@gmail.com>
2022-01-27 16:16:32 -0300

    Show spinner while sort request is performed

--- app/assets/javascripts/sortable_list.js
+++ app/assets/javascripts/sortable_list.js
@@ -1,10 +1,15 @@
 document.addEventListener("turbolinks:load", function () {
   $("#stories, .sortable-projects").sortable({
     update: function (e, ui) {
+      const project = ui.item[0].closest("div.project-card");
+      project.classList.add("sorting");
       Rails.ajax({
         type: "PATCH",
         url: this.dataset.url,
         data: $(this).sortable("serialize"),
+        complete: () => {
+          project.classList.remove("sorting");
+        },
       });
     },
   });
--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -2,14 +2,14 @@ require "rails_helper"
 
 RSpec.describe "home specs" do
   let(:user) { FactoryBot.create(:user) }
-  
+
   before do
     login_as(user, scope: :user)
   end
 
   context "with projects" do
     let!(:project) { FactoryBot.create(:project) }
-    
+
     context "with sub projects" do
       let!(:sub_project1) { FactoryBot.create(:project, parent: project, position: 1) }
       let!(:sub_project2) { FactoryBot.create(:project, parent: project, position: 2) }
@@ -18,14 +18,13 @@ RSpec.describe "home specs" do
       it "allows re-arranging the sub projects", js: true do
         visit root_path
 
-        first = find('a', text: sub_project1.title)
-        last = find('a', text: sub_project3.title)
+        first = find("a", text: sub_project1.title)
+        last = find("a", text: sub_project3.title)
 
         # this will move `last` BEFORE `first`
         last.drag_to(first)
-
-        # there's no indication on the page that the `sort` request finished, so we have to use `sleep`
-        sleep(1)
+        expect(page).to have_selector("#project_#{project.id}.project-card.sorting")
+        expect(page).not_to have_selector("#project_#{project.id}.project-card.sorting")
 
         expect(sub_project1.reload.position).to eq 2
         expect(sub_project2.reload.position).to eq 3
