3016f4878f9fee081d9f3c22d4297dd3f0f84442
Ariel Juodziukynas <arieljuod@gmail.com>
2022-02-14 14:57:56 -0300

    Remove confitional

--- app/assets/javascripts/sortable_list.js
+++ app/assets/javascripts/sortable_list.js
@@ -2,13 +2,13 @@ document.addEventListener("turbolinks:load", function () {
   $("#stories, .sortable-projects").sortable({
     update: function (e, ui) {
       const parent = ui.item[0].closest("div.project-card, .project-table");
-      if (parent) parent.classList.add("sorting");
+      parent.classList.add("sorting");
       Rails.ajax({
         type: "PATCH",
         url: this.dataset.url,
         data: $(this).sortable("serialize"),
         complete: () => {
-          if (parent) parent.classList.remove("sorting");
+          parent.classList.remove("sorting");
         },
       });
     },
