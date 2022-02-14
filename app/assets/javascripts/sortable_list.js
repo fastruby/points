744ac87f994dab0ff0ba882c851e7d4d5e1a556a
Ariel Juodziukynas <arieljuod@gmail.com>
2022-02-14 14:55:17 -0300

    Fix sorting stories and add test

--- app/assets/javascripts/sortable_list.js
+++ app/assets/javascripts/sortable_list.js
@@ -1,14 +1,14 @@
 document.addEventListener("turbolinks:load", function () {
   $("#stories, .sortable-projects").sortable({
     update: function (e, ui) {
-      const project = ui.item[0].closest("div.project-card");
-      project.classList.add("sorting");
+      const parent = ui.item[0].closest("div.project-card, .project-table");
+      if (parent) parent.classList.add("sorting");
       Rails.ajax({
         type: "PATCH",
         url: this.dataset.url,
         data: $(this).sortable("serialize"),
         complete: () => {
-          project.classList.remove("sorting");
+          if (parent) parent.classList.remove("sorting");
         },
       });
     },
