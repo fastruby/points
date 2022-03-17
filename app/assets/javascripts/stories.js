7de41a7dc703ca5b78423d87de26274fdb9a5d87
Brian E. McElaney <brian.e.mcelaney@gmail.com>
2022-03-16 22:22:35 -0400

    Set up a generic way to handle markdown previews

--- app/assets/javascripts/stories.js
+++ app/assets/javascripts/stories.js
@@ -1,23 +1,17 @@
 document.addEventListener("input", (e) => {
-  function preview_setup(text_area_class, preview_class) {
-    if (e.target.classList.contains(text_area_class)) {
-      const desc = e.target;
-      const form = desc.closest("form");
-      const preview = form.querySelector(preview_class  + " .content");
-      if (preview) {
-        Rails.ajax({
-          type: "POST",
-          url: "/stories/render_markdown",
-          data: `markdown=${encodeURIComponent(desc.value)}`,
-          dataType: "text",
-          success: (response) => {
-            preview.innerHTML = response;
-          },
-        });
-      }
+  document.querySelectorAll('[data-has-preview]').forEach(element => {
+    const form = element.closest("form");
+    const preview = form.querySelector("." + element.dataset.previewTarget  + " .content")
+    if (preview) {
+      Rails.ajax({
+        type: "POST",
+        url: "/stories/render_markdown",
+        data: `markdown=${encodeURIComponent(element.value)}`,
+        dataType: "text",
+        success: (response) => {
+          preview.innerHTML = response;
+        },
+      });
     }
-  }
-
-  preview_setup("story-description", ".story_preview")
-  preview_setup("story-extra-info", ".extra_info_preview")
+  })
 });
