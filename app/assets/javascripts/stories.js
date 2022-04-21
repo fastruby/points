9e399279630699e14b2317f04016ce88620213f0
Francois Buys <buys.fran@gmail.com>
2022-04-21 15:43:04 +0200

    Debounce story input events
    So capybara tests don't fail due to a race condition between input
    events fired and the server's ability to render markup.
    
    Adding this debounce makes our tests more reliable and reduces server
    load when users make use of this markdown functionality.

--- app/assets/javascripts/stories.js
+++ app/assets/javascripts/stories.js
@@ -1,19 +1,25 @@
 document.addEventListener("input", (e) => {
-  document.querySelectorAll("[data-has-preview]").forEach((element) => {
-    const form = element.closest("form");
-    const preview = form.querySelector(
-      "." + element.dataset.previewTarget + " .content"
-    );
-    if (preview) {
-      Rails.ajax({
-        type: "POST",
-        url: "/stories/render_markdown",
-        data: `markdown=${encodeURIComponent(element.value)}`,
-        dataType: "text",
-        success: (response) => {
-          preview.innerHTML = response;
-        },
-      });
-    }
-  });
+  const updateMarkdown = () => {
+    document.querySelectorAll("[data-has-preview]").forEach((element) => {
+      const form = element.closest("form");
+      const preview = form.querySelector(
+        "." + element.dataset.previewTarget + " .content"
+      );
+      if (preview) {
+        Rails.ajax({
+          type: "POST",
+          url: "/stories/render_markdown",
+          data: `markdown=${encodeURIComponent(element.value)}`,
+          dataType: "text",
+          success: (response) => {
+            preview.innerHTML = response;
+          },
+        });
+      }
+    });
+  };
+
+  var debounceTimer;
+  window.clearTimeout(debounceTimer);
+  debounceTimer = window.setTimeout(updateMarkdown, 300);
 });
