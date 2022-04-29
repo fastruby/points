006684170f42573fd8854023e19aed5c367490ed
rishijain <jainrishi.37@gmail.com>
2022-04-29 16:50:50 +0530

    removed conditional config as they are anyways not supported in rails 7.1+

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,10 +39,4 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  # In Rails 6.1+, Active Record provides a new internal API for connection management.
-  # for more details check here
-  # https://guides.rubyonrails.org/active_record_multiple_databases.html#migrate-to-the-new-connection-handling
-  if Rails::VERSION::MAJOR == 7 && Rails::VERSION::MINOR == 0
-    config.active_record.legacy_connection_handling = false
-  end
 end
