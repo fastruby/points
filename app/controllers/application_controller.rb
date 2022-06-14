6f233fec1f9d536f99c0baae48dcf15caf560317
Ariel Juodziukynas <arieljuod@gmail.com>
2022-06-14 16:15:58 -0300

    enable RMP for every request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :enable_rack_mini_profiler
 
   def hello
     render html: "hello, world!"
@@ -7,5 +8,12 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     projects_path
+  end
+
+  private
+
+  # we can enable this for everybody, it's hidden by default
+  def enable_rack_mini_profiler
+    Rack::MiniProfiler.authorize_request
   end
 end
