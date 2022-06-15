6b9192e42665d7474cd24872ea0fa260439a0f7b
Ariel Juodziukynas <arieljuod@gmail.com>
2022-06-15 10:16:25 -0300

    Enable/disable RMP with a cookie, README

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :enable_rack_mini_profiler
+  before_action :rack_mini_profiler
 
   def hello
     render html: "hello, world!"
@@ -13,7 +13,18 @@ class ApplicationController < ActionController::Base
   private
 
   # we can enable this for everybody, it's hidden by default
-  def enable_rack_mini_profiler
-    Rack::MiniProfiler.authorize_request
+  def rack_mini_profiler
+    show =
+      if params.key?(:enable_rack_mini_profiler)
+        cookies[:rack_mini_profiler] = "show"
+        true
+      elsif params.key?(:disable_rack_mini_profiler)
+        cookies.delete(:rack_mini_profiler)
+        false
+      end
+
+    if show || cookies[:rack_mini_profiler]
+      Rack::MiniProfiler.authorize_request
+    end
   end
 end
