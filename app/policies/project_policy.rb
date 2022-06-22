e0e703f2b634dbedb83294f328618403220b56b8
aysan <aisayo@icloud.com>
2022-06-22 16:01:23 -0400

    Created project policy

---
+++ app/policies/project_policy.rb
@@ -0,0 +1,14 @@
+class ProjectPolicy < ApplicationPolicy
+
+  attr_reader :user, :project
+
+  def initialize(user, project)
+    @user = user 
+    @project = project
+  end 
+
+  # this is so that the project can be edited
+  def update?
+    !project.locked?
+  end 
+end 
---
+++ spec/policies/project_policy_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+describe ProjectPolicy do
+  subject { described_class }
+
+  permissions :update? do
+    let(:user) { FactoryBot.build(:user) }
+    let(:locked_project) do 
+      FactoryBot.build(:project, locked: Time.current)
+    end 
+    let(:project) do 
+      FactoryBot.build(:project, locked: nil)
+    end 
+
+    it "denies access if project is locked" do
+      expect(subject).not_to permit(user, locked_project)
+    end
+  
+    it "grants access if project is not locked" do
+      expect(subject).to permit(user, project)
+    end
+  end 
+end
