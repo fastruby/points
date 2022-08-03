e3fd89af7c6b238eb778b195643e818400cd1928
aisayo <aisayo@icloud.com>
2022-08-03 09:06:21 -0400

    DT-328 unlock a project (#273)
    * Update to toggle unlocked project
    
    * Add Unlock Project button
    
    * Add spec for unlock project if user is admin
    
    * Remove whitespace
    
    * Refactor find projects logic to before_action
    
    * Remove white space
    
    * Update spec to include scope of container
    
    * undo change made to expect_buttons_to_be_hidden
    
    * Remove white space

--- spec/features/projects_manage_spec.rb
+++ spec/features/projects_manage_spec.rb
@@ -336,7 +336,6 @@ RSpec.describe "managing projects", js: true do
   end
 
   describe "when locking a project" do
-
     context "when a user is an admin" do
       let(:user) { FactoryBot.create(:user, admin: true) }
 
@@ -372,6 +371,12 @@ RSpec.describe "managing projects", js: true do
           visit project_path(id: locked_project.id)
 
           expect_buttons_to_be_hidden
+        end
+
+        it "does not render unlocked button" do
+          visit project_path(id: project.id)
+
+          expect(page).to have_no_selector(:link_or_button, "Unlock Project")
         end
       end
     end
@@ -423,6 +428,32 @@ RSpec.describe "managing projects", js: true do
         end
 
         expect(page).to have_button("Bulk Delete", disabled: true)
+      end
+    end
+  end
+
+  describe "when unlocking a project" do
+    context "when a user is an admin" do
+      let(:user) { FactoryBot.create(:user, admin: true) }
+
+      context "if a project is locked" do
+        let(:locked_project) { FactoryBot.create(:project, :locked) }
+
+        it "shows an unlocked button" do
+          visit project_path(locked_project)
+          within "div.btn-group.actions" do
+            expect(page).to have_text("Unlock Project")
+          end
+        end
+
+        it "unlocks a project when 'Unlock Project' is clicked" do
+          visit project_path(locked_project)
+          click_button "Unlock Project"
+          within "div.btn-group.actions" do
+            expect(page).to have_text("Lock Project")
+            expect(page).not_to have_text("Unlock Project")
+          end
+        end
       end
     end
   end
