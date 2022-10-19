8b63265264bfe4ec6bafc12841925a6be6b057c3
Tiago Casanova <tiago.casanova.pt@gmail.com>
2022-10-19 19:03:26 +0100

    Define after_sign_in_path_for Devise inside the gem to use the main app root URL

--- lib/ombu_labs-auth/app/controllers/ombu_labs/auth/application_controller.rb
+++ lib/ombu_labs-auth/app/controllers/ombu_labs/auth/application_controller.rb
@@ -1,6 +1,9 @@
 module OmbuLabs
   module Auth
     class ApplicationController < ActionController::Base
+      def after_sign_in_path_for(resource)
+        request.base_url
+      end
     end
   end
 end
