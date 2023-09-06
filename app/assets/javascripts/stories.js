96ba4f8acf3932146da19245d44b5c2454b68862
Ariel Juodziukynas <arieljuod@gmail.com>
2023-09-06 14:48:02 -0300

    ROAD-539 Fix debounce of inputs' preview (#302)

--- app/assets/javascripts/stories.js
+++ app/assets/javascripts/stories.js
@@ -1,25 +1,28 @@
-document.addEventListener("input", (e) => {
-  const updateMarkdown = () => {
-    document.querySelectorAll("[data-has-preview]").forEach((element) => {
-      const form = element.closest("form");
-      const preview = form.querySelector(
-        "." + element.dataset.previewTarget + " .content"
-      );
-      if (preview) {
-        Rails.ajax({
-          type: "POST",
-          url: "/stories/render_markdown",
-          data: `markdown=${encodeURIComponent(element.value)}`,
-          dataType: "text",
-          success: (response) => {
-            preview.innerHTML = response;
-          },
-        });
-      }
-    });
-  };
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll("[data-has-preview]").forEach((element) => {
+    let debounceTimer;
 
-  var debounceTimer;
-  window.clearTimeout(debounceTimer);
-  debounceTimer = window.setTimeout(updateMarkdown, 300);
+    element.addEventListener("input", (e) => {
+      const updateMarkdown = () => {
+        const form = element.closest("form");
+        const preview = form.querySelector(
+          "." + element.dataset.previewTarget + " .content"
+        );
+        if (preview) {
+          Rails.ajax({
+            type: "POST",
+            url: "/stories/render_markdown",
+            data: `markdown=${encodeURIComponent(element.value)}`,
+            dataType: "text",
+            success: (response) => {
+              preview.innerHTML = response;
+            },
+          });
+        }
+      };
+
+      window.clearTimeout(debounceTimer);
+      debounceTimer = window.setTimeout(updateMarkdown, 300);
+    });
+  });
 });
