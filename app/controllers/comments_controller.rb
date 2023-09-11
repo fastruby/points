7d6c1413268f406a22c06791f727e9aca2ec8ee9
G. Torres <gelsey@ombulabs.com>
2023-09-11 17:31:18 -0400

    Adds comment model and controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,69 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :load_story_and_project
+  before_action :find_comment, only: [:edit, :update, :destroy]
+
+  def edit
+  end
+
+  def create
+    @comment = current_user.comments.build(story: @story)
+    @comment.attributes = comment_params
+    @comment.user_id = current_user.id
+    saved = @comment.save
+    respond_to do |format|
+      format.html do
+        if saved
+          flash[:success] = "Comment created!"
+          redirect_to story_path(@comment.story_id)
+        else
+          flash[:error] = @comment.errors.full_messages
+          render :new
+        end
+      end
+      format.js
+    end
+  end
+
+  def update
+    updated = @comment.update(comment_params)
+    respond_to do |format|
+      format.html do
+        if updated
+          flash[:success] = "Comment updated!"
+          redirect_to story_path(@comment.story_id)
+        else
+          flash[:error] = @comment.errors.full_messages
+          render :edit
+        end
+      end
+      format.js
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    respond_to do |format|
+      format.html { redirect_to story_path(@comment.story_id), notice: "comment was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def find_comment
+    @comment = current_user.comments.where(story_id: params[:story_id]).find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:error] = "Comment not found"
+    redirect_to story_path(params[:story_id])
+  end
+
+  def load_story_and_project
+    @project = Project.find(params[:project_id])
+    @story = Story.find(params[:story_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+end
