cf3af58a2f96aac9fa53de7ba4fca946da4334ba
Mateus Pereira <mateus@ombulabs.com>
2023-10-11 15:23:57 -0300

    Remove Rejected as an option on story creation

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -9,4 +9,10 @@ module StoriesHelper
 
     "orange"
   end
+
+  def options_for_status_select(story, action)
+    return options_for_select({"Pending" => "pending", "Approved" => "approved"}, selected: story.status) if action == "new"
+
+    options_for_select({"Pending" => "pending", "Approved" => "approved", "Rejected" => "rejected"}, selected: story.status)
+  end
 end
