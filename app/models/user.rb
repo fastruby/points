86378e04c5cb715792a4a82131bc43fec64e10fb
Fiona <fionadlapham@gmail.com>
2024-02-08 15:40:16 -0500

    Update according to feedback

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,4 @@ class User < ApplicationRecord
   has_many :estimates
   has_many :comments
 
-  # Returns the email if the user doesn't have a name in Github
-  def name
-    read_attribute(:name) || email
-  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -34,11 +34,6 @@ RSpec.describe User, type: :model do
       it "returns the name a name exists" do
         expect(subject.name).to eql subject.name
       end
-
-      it "returns the email if the name doesn't exist" do
-        subject.name = nil
-        expect(subject.name).to eql subject.email
-      end
     end
   end
 end
