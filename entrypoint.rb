fcdce1021e2d8c379561ca14fd49928427c6195d
Mateus Pereira <mateus@ombulabs.com>
2025-02-12 14:19:00 -0300

    Fix rubycritic and skunk calls to save artifacts in the right folder

--- entrypoint.rb
+++ entrypoint.rb
@@ -3,11 +3,18 @@
 # Get arguments passed to the container
 commands = ARGV
 
+def run_rubycritic
+  system("rubycritic -p reports/rubycritic")
+end
+
+def run_skunk
+  system("skunk -o reports/skunk.txt")
+end
+
 # Default behavior: Run both tools if no args are provided
 if commands.empty?
   puts "No arguments provided. Running both RubyCritic and Skunk..."
-  system("rubycritic")
-  system("skunk -o skunk.txt")
+  run_rubycritic
   exit 0
 end
 
@@ -16,10 +23,10 @@ commands.each do |command|
   case command
   when "rubycritic"
     puts "Running RubyCritic..."
-    system("rubycritic")
+    run_rubycritic
   when "skunk"
     puts "Running Skunk..."
-    system("skunk -o skunk.txt")
+    run_skunk
   else
     puts "Invalid argument: #{command}"
     puts "Usage: docker run --rm <image> [rubycritic] [skunk]"
